ca1ffe441e2351b35576f5b5e4ef40d3840f1bd5
Alexander <independent9@ya.ru>
2017-04-22 20:35:41 +0000

    added reset password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_create :create_activation_digest
     VALID_NAME = /\A[a-zA-Z]+\z/
     VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -37,4 +37,19 @@ class User < ActiveRecord::Base
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+    
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest,  User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+    
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+    
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+    end
+    
 end
