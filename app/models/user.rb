bc0d3b446694f5cc355c3436003f0a94731f942c
Alexander <independent9@ya.ru>
2017-04-24 03:04:18 +0000

    small changes in email

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,15 @@ class User < ActiveRecord::Base
         update_attribute(:remember_digest, nil)
     end
     
+    def activate
+        update_attribute(:activated,    true)
+        update_attribute(:activated_at, Time.zone.now)
+    end
+    
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
+    end
+    
     def create_activation_digest
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
